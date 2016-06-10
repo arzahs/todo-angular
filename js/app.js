76cff0f48d6ce9b5bc53af2e1247086098cf2925
Sergey Nelepa <nelepa1995@mail.ru>
2016-06-10 13:57:42 +0300

    added fun for adding task

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 var todoApp = angular.module('todoApp', []);
 
 var model = [
-  {task: 'test', status: false},
+  {task: 'test', status: true},
   {task: 'test1', status: false}
 ];
 
 todoApp.controller('ListCtrl', function ($scope){
   $scope.items = model;
+
+  $scope.addTask = function () {
+    model.push({task: $scope.newTask, status: false});
+    $scope.newTask = '';
+  }
+
+  $scope.newTask = '';
 
 });
