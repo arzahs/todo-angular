dde8e54331a307398309263fce33553139054457
Sergey Nelepa <nelepa1995@mail.ru>
2016-06-10 14:10:22 +0300

    added saving to localStorage

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 var todoApp = angular.module('todoApp', []);
 
-var model = [
-  {task: 'test', status: true},
-  {task: 'test1', status: false}
-];
+// var model = [
+//   {task: 'test', status: true},
+//   {task: 'test1', status: false}
+// ];
+if(localStorage.tasks){
+  var model = angular.fromJson(localStorage.tasks);
+}else {
+  var model = [];
+}
+
+
 
 todoApp.controller('ListCtrl', function ($scope){
   $scope.items = model;
@@ -11,6 +18,11 @@ todoApp.controller('ListCtrl', function ($scope){
   $scope.addTask = function () {
     model.push({task: $scope.newTask, status: false});
     $scope.newTask = '';
+    $scope.saveStorage();
+  }
+
+  $scope.saveStorage = function(){
+    localStorage.tasks = angular.toJson(model);
   }
 
   $scope.newTask = '';
